381ace8fd300a77eef9ed01df59edd30c72506e5
B3XED <50170063+B3XED@users.noreply.github.com>
2020-05-01 19:16:37 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+    ['invalid_amount'] = 'Nieprawidłowa kwota',
+    ['atm_blip'] = 'Banktomat',
+    ['bank_blip'] = 'Bank',
+    ['atm_open'] = 'Naciśnij ~INPUT_PICKUP~ aby uzyskać dostęp do konta ~b~',
+    ['no_money'] = 'Nie masz wystarczająco pieniędzy',
+    ['recieved1'] = 'Otrzymałeś pieniądze ',
+    ['recieved2'] = 'Otrzymałes sumę ',
+    ['recieved3'] = ' dolarów',
+    ['removed1'] = 'Potwierdzenie wpłaty ',
+    ['removed2'] = 'Twoja płatność ',
+    ['removed3'] = ' dolarów została zrealizowana',
+}
---
+++ server.lua
@@ -0,0 +1,106 @@
+-- ================================================================================================--
+-- ==                                VARIABLES - DO NOT EDIT                                     ==--
+-- ================================================================================================--
+ESX = nil
+
+function getIdentity(source)
+	local identifier = GetPlayerIdentifiers(source)[1]
+	local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {['@identifier'] = identifier})
+	if result[1] ~= nil then
+		local identity = result[1]
+
+		return {
+			identifier = identity['identifier'],
+			firstname = identity['firstname'],
+			lastname = identity['lastname'],
+			dateofbirth = identity['dateofbirth'],
+			sex = identity['sex'],
+			height = identity['height']
+			
+		}
+	else
+		return nil
+	end
+end
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('bank:deposit')
+AddEventHandler('bank:deposit', function(amount)
+    local _source = source
+
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
+        TriggerClientEvent('chatMessage', _source, _U('invalid_amount'))
+    else
+        xPlayer.removeMoney(amount)
+        xPlayer.addAccountMoney('bank', tonumber(amount))
+    end
+end)
+
+RegisterServerEvent('bank:withdraw')
+AddEventHandler('bank:withdraw', function(amount)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local base = 0
+    amount = tonumber(amount)
+    base = xPlayer.getAccount('bank').money
+    if amount == nil or amount <= 0 or amount > base then
+        TriggerClientEvent('chatMessage', _source, _U('invalid_amount'))
+    else
+        xPlayer.removeAccountMoney('bank', amount)
+        xPlayer.addMoney(amount)
+    end
+end)
+
+RegisterServerEvent('bank:balance')
+AddEventHandler('bank:balance', function()
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    balance = xPlayer.getAccount('bank').money
+    local name = getIdentity(_source)
+    TriggerClientEvent('currentbalance1', _source, balance, name.firstname, name.lastname)
+end)
+
+RegisterServerEvent('bank:transfer')
+AddEventHandler('bank:transfer', function(to, amountt)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local zPlayer = ESX.GetPlayerFromId(to)
+    local balance = 0
+    if zPlayer ~= nil then
+        balance = xPlayer.getAccount('bank').money
+        zbalance = zPlayer.getAccount('bank').money
+        if tonumber(_source) == tonumber(to) then
+            -- advanced notification with bank icon
+            TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank',
+                               'Transfer Money',
+                               'You cannot transfer to your self!',
+                               'CHAR_BANK_MAZE', 9)
+        else
+            if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <=
+                0 then
+                -- advanced notification with bank icon
+                TriggerClientEvent('esx:showAdvancedNotification', _source,
+                                   'Bank', 'Transfer Money',
+                                   'Not enough money to transfer!',
+                                   'CHAR_BANK_MAZE', 9)
+            else
+                xPlayer.removeAccountMoney('bank', tonumber(amountt))
+                zPlayer.addAccountMoney('bank', tonumber(amountt))
+                -- advanced notification with bank icon
+                TriggerClientEvent('esx:showAdvancedNotification', _source,
+                                   'Bank', 'Transfer Money',
+                                   'You transfered ~r~$' .. amountt ..
+                                       '~s~ to ~r~' .. to .. ' .',
+                                   'CHAR_BANK_MAZE', 9)
+                TriggerClientEvent('esx:showAdvancedNotification', to, 'Bank',
+                                   'Transfer Money', 'You received ~r~$' ..
+                                       amountt .. '~s~ from ~r~' .. _source ..
+                                       ' .', 'CHAR_BANK_MAZE', 9)
+            end
+
+        end
+    end
+
+end)
